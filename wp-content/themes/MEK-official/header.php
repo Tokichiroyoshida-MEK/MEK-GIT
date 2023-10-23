d70fdc877c6b493eacdba2ff9f213c255775d0a5
keita0815 <fukazawa.keita@tokiwa-official.com>
2023-10-23 16:06:09 +0900

    深澤20231023

--- wp-content/themes/MEK-official/header.php
+++ wp-content/themes/MEK-official/header.php
@@ -20,11 +20,15 @@
 <body <?php body_class(); ?>><?php wp_body_open(); ?>
 <header>
 <!-- ここにヘッダー記述 -->
-<header class="header">
-    <div class="header_kv">
-        <h1 class="header_kv_logo">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="鮨まるふくロゴ写真">
-        </h1>
-    </div>
-</header>
+<header>
+    <header class="header">
+        <div class="header_kv">
+            <h1 class="header_kv_logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="">
+            </h1>
+        </div>
+        <div class="header_kv_text">
+            <h2 class="header_kv_text_txt">温かみと高級感が<br>共存する鮨の宿</h2>
+        </div>
+    </header>
 </header>
