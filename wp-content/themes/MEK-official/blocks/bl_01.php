b47795d79003c1cb02547a389b2c5d2f2967e133
keita0815 <fukazawa.keita@tokiwa-official.com>
2023-11-20 18:25:38 +0900

    20231120 テキストなど入れました

--- wp-content/themes/MEK-official/blocks/bl_01.php
+++ wp-content/themes/MEK-official/blocks/bl_01.php
@@ -2,8 +2,8 @@
     <header class="header">
         <div class="header_kv_text">
             <h1 class="header_kv_text_txt fadein">
-                温かみと高級感が<br>
-                共存する鮨の宿
+            究極の旨味を
+            <br>追求する熟成鮨
             </h1>
         </div>
     </header>
